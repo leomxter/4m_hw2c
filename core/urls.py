ea9c0bbef627abbe825af40e7e04b43da6e1d158
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-02-21 21:56:59 +0600

    first commit

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("posts.urls")),    
-]
-
-urlpatterns1 = [
-    path('admin/', admin.site.urls),
-    path("", include("users.urls")),    
+    path("", include("posts.urls")),
+    path("users/", include("users.urls")),    
+    
 ]
