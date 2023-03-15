f65801f41aa513f162e72f3b72493b6b6dea8545
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-03-15 18:27:09 +0600

    hw7c

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from users.views import register
+from users.views import RegisterView
 
 urlpatterns = [
-    path("users/register/", register, name="register"),
+    path("users/register/", RegisterView.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
-from .forms import UserRegistrationForm
+from users.forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views import generic
 
+# def register(request):
+#     if request.method == "POST":
+#         user_form = UserRegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             new_user = user_form.save(commit=False)
+#             new_user.set_password(user_form.cleaned_data["password"])
+#             new_user.save()
+#             return render(request, "registration/register_done.html", {"user": new_user})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, "registration/register.html", {"form": user_form})
 
-def register(request):
-    if request.method == "POST":
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data["password"])
-            new_user.save()
-            return render(request, "registration/register_done.html", {"user": new_user})
-    else:
-        user_form = UserRegistrationForm()
-    return render(request, "registration/register.html", {"form": user_form})
+class RegisterView(generic.CreateView):
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return render(request,"registration/register_done.html", {"user": new_user})
+        else:
+            return render(request, "registration/register.html", {"form": form})
+
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registration/register.html", {"form": form})
+
